e51bdd563fd911cda9097d4142ef2ee28a27a9ba
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-03-28 19:01:26 +0530

    Time: 4 ms (30.16%) | Memory: 56.5 MB (58.57%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function(nums) {
+      let l = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+        nums[i] = nums[i] * 2;
+        nums[i + 1] = 0;
+      }
+    if (nums[i] !== 0) {
+      [nums[i], nums[l]] = [nums[l], nums[i]];
+      l++;
+    }
+  }
+  return nums;
+};
