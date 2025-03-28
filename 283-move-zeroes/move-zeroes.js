53efd1b9b458e86faf7ae5c9b150f71f293f8ef7
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-03-28 09:38:38 +0530

    Time: 6 ms (25.08%) | Memory: 60.8 MB (18.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -2,14 +2,15 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-     if (nums.length === 0) return nums;
-  let l = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      [nums[i], nums[l]] = [nums[l], nums[i]];
-      l++;
+var moveZeroes = function (nums) {
+    if (nums.length<=1) return nums;
+    let l = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            
+            [nums[i], nums[l]] = [nums[l], nums[i]];
+            l++;
+        }
     }
-  }
-  return nums;
+    return nums;
 };
