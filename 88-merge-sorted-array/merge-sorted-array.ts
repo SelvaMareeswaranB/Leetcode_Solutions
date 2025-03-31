78f0fe046901ff9faaf0b5fd2aa6a56ac29b83cf
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-03-31 17:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 56.9 MB (16.87%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -0,0 +1,16 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let k: number = m + n - 1;
+    let i: number = m - 1;
+    let j: number = n - 1;
+    
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+}
