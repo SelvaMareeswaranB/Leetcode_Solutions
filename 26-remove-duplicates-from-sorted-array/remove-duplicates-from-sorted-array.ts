e08348319c45cc1b3608f3ee4662e1cbb6f7f8b7
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-04-02 13:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 59.1 MB (28.55%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,10 +1,10 @@
 function removeDuplicates(nums: number[]): number {
-   let k =0;
+   let k =1;
     for(let i=1;i<nums.length;i++){
         if(nums[i] !== nums[i-1]){
-           nums[++k]=nums[i]
+           nums[k++]=nums[i]
         
         }
     }
-    return k+1;
+    return k;
 };
