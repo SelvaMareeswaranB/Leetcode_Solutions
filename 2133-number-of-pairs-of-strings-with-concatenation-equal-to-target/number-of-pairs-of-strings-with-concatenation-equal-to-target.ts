11abb9ab80a72aac6d0167d37b88fb4f6750e048
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-04-16 15:17:01 +0530

    Time: 7 ms (83.33%) | Memory: 60.1 MB (83.33%) - LeetSync

---
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.ts
@@ -0,0 +1,11 @@
+function numOfPairs(nums, target) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i !== j && nums[i] + nums[j] === target) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
