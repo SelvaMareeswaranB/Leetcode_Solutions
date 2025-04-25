5bada89b5cd30359f335377b444d2f591bfafe2f
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-04-25 18:29:56 +0530

    Time: 729 ms (5.13%) | Memory: 65.1 MB (5.03%) - LeetSync

---
+++ 383-ransom-note/ransom-note.ts
@@ -0,0 +1,12 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  let cache = Array(26).fill(0);
+  for (let i = 0; i < ransomNote.length; i++) {
+    cache[ransomNote.charCodeAt(i) - 97]++
+  }
+
+  for (let i = 0; i < magazine.length; i++) {
+    console.log(magazine.charCodeAt(i) - 97)
+    cache[magazine.charCodeAt(i) - 97]--
+  }
+  return cache.every((ele)=>ele<1);
+}
