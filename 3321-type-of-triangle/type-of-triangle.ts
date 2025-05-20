2afdf86dba3dfcc70da9e35e28c85b1547cc945f
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-05-20 18:15:36 +0530

    Time: 1 ms (38.24%) | Memory: 57.5 MB (50.00%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.ts
@@ -0,0 +1,9 @@
+function triangleType(nums: number[]): string {
+    const [a, b, c] = nums;
+
+    if (a + b <= c || a + c <= b || b + c <= a) return "none";
+
+    if (a === b && b === c) return "equilateral";
+    if (a === b || b === c || a === c) return "isosceles";
+    return "scalene";
+}
