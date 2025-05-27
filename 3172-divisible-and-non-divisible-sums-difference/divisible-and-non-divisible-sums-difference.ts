7aa35a7685e5bad85dc7e35dcaf0d59367996562
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-05-27 08:53:50 +0530

    Time: 1 ms (33.90%) | Memory: 57.8 MB (30.51%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.ts
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.ts
@@ -1,10 +1,13 @@
 function differenceOfSums(n: number, m: number): number {
     let num1 = 0
     let num2 = 0
-    for(let i=1;i<=n;i++){
-        if(i%m !== 0) num1+=i
-        else num2+=i
+    if (n < m) {
+        return n * (n + 1) / 2
+    }
+    for (let i = 1; i <= n; i++) {
+        if (i % m !== 0) num1 += i
+        else num2 += i
     }
 
-    return  num1 - num2
+    return num1 - num2
 };
