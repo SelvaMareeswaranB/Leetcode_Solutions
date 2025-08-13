b2d285dca9ab7dad933cdbac0276683c39e15fc6
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-08-13 07:36:07 +0530

    Time: 2 ms (74.52%) | Memory: 64.4 MB (62.55%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.ts
@@ -0,0 +1,13 @@
+function maxSubArray(nums: number[]): number {
+  let sum=0;
+ let max=-Infinity
+
+  for(let i=0;i<nums.length;i++){
+    sum+=nums[i]
+    max=Math.max(sum,max)
+    if(sum < 0) sum=0
+ 
+  }
+
+  return max
+}
