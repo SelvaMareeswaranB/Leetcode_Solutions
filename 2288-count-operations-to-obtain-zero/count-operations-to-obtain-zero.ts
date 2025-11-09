2d9e20e3df9177471ac25da53ec088a0066b9148
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2025-11-09 10:40:36 +0530

    Time: 5 ms (25.49%) | Memory: 55.9 MB (45.10%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.ts
@@ -0,0 +1,15 @@
+function countOperations(num1: number, num2: number): number {
+  let opt = 0
+  while(num1*num2 !== 0){
+    if(num1>=num2){
+      num1=num1-num2
+      opt++
+    }
+    else{
+      num2=num2-num1
+         opt++
+    }
+  }
+  return opt
+    
+};
