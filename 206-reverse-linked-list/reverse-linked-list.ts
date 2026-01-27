e76228a6ac7e2b2b2b7568afaa579224074712af
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2026-01-27 23:21:09 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (94.42%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let tempCurr = null;
+    let tempPrev = null;
+    let curr = head;
+    let prev = null;
+
+    while (curr) {
+      tempCurr = curr.next;
+      tempPrev = curr;
+      curr.next = prev;
+      prev = tempPrev;
+      curr = tempCurr;
+    }
+
+    return prev;  
+};
