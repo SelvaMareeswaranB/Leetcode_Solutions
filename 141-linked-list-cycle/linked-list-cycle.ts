fa9e802a7345f168f2e96d23e1432487a0a73e2f
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2026-01-28 18:05:51 +0530

    Time: 61 ms (6.78%) | Memory: 58.8 MB (22.67%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+        let slow: ListNode | null = head;
+    let fast: ListNode | null = head?.next ?? null;
+    let isCycleDeteced = false;
+    while (fast || slow) {
+      if (slow === fast) {
+        isCycleDeteced = true;
+        break;
+      }
+      slow = slow?.next ?? null;
+      fast = fast?.next?.next ?? null;
+    }
+    return isCycleDeteced;
+};
