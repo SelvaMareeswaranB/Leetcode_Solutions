d093e677842d8cf95157559ff119c499a68721a6
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2026-05-30 07:27:22 +0530

    Time: 43 ms (16.95%) | Memory: 70.2 MB (16.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,20 +1,13 @@
 function groupAnagrams(strs: string[]): string[][] {
-  let result: string[][] = [];
-  let hashtable = new Map<string, string[]>();
-  for (let i = 0; i < strs.length; i++) {
-    let sorted = strs[i].split("").sort().join("")
-    let key = hashtable.get(sorted)
-    if(key){
-     hashtable.set(sorted,[...key,strs[i]])
-    }
-    else{
-      hashtable.set(sorted,[strs[i]])
-    }
-  }
+    const group:{[key:string]:string[]} = {}
 
-  hashtable.forEach((value,key)=>{
-  
-    result.push(value)
-  })
-  return result;
-}
+    for (let word of strs) {
+        const key = [...word].sort().join("")
+
+        if (!group[key]) group[key] = []
+        group[key].push(word)
+    }
+    return Object.values(group)
+
+
+};
