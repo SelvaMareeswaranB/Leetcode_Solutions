bbe545f8fa2dc3f091bde7551a618551a8469a1c
Selva Mareeswaran B <139097570+SelvaMareeswaranB@users.noreply.github.com>
2026-06-01 08:08:34 +0530

    Time: 11 ms (64.03%) | Memory: 60.4 MB (61.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,24 +1,15 @@
-function topKFrequent(nums: number[], k: number): number[] {
-    let cache = new Map();
-    let result: number[] = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (cache.has(nums[i])) {
-            let count = cache.get(nums[i]);
-            count++;
-
-            cache.set(nums[i], count);
-        }
-        else {
-            cache.set(nums[i], 1);
-        }
-
+function topKFrequent(arr: number[], k: number): number[] {
+      let map =new Map<number,number>()
+    let result =[]
+    
+    for(let el of arr){
+        map.set(el,(map.get(el) ?? 0) + 1)
     }
-    const sortedMap = new Map([...cache].sort((a, b) => b[1] - a[1]));
-    const resultant = [...sortedMap]
-
-    for (let i = 0; i < k; i++) {
-        result.push(resultant[i][0])
+    
+    const entries = [...map]
+    entries.sort((a, b) => b[1] - a[1]);
+    for(let i=0;i<k;i++){
+        result.push(entries[i][0])
     }
-
-    return result;
-}
+    return result
+};
